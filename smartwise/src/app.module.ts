32c7bc5db0005bd5d525b4919231c47ef380291a
Naveen-unni <naveenunni403@gmail.com>
2026-08-08 13:00:35 +0530

    add code

---
+++ smartwise/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+     TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'smartwise.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),UsersModule],
+
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
